054d97ee18e9808ff6639e177d3b467b4e753006
swwdxsama <mailssabox@gmail.com>
2024-12-29 20:20:51 +0200

    Update: New functions for doctorView

--- src/DoctorView.java
+++ src/DoctorView.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.*;
@@ -14,9 +16,11 @@ public class DoctorView extends JFrame {
     private List<String[]> getAppointments(Integer UserID) {
         List<String[]> appointments = new ArrayList<>();
         String query = """
-        SELECT AppointmentID, Patient, User, Service, DateTime, Status, Notes
-        FROM appointments
-        WHERE User = ?
+        
+                SELECT a.AppointmentID, a.Patient, a.User, a.Service, a.DateTime, a.Status, a.Notes, p.FirstName, p.LastName
+                FROM appointments a
+                Join pacients p on a.Patient = p.PacientID
+                WHERE a.User = ?
         """;
 
         try (Connection conn = DriverManager.getConnection(url, uid, pw);
@@ -33,7 +37,9 @@ public class DoctorView extends JFrame {
                             String.valueOf(rs.getInt("Service")),
                             String.valueOf(rs.getTimestamp("DateTime")),
                             rs.getString("Status"),
-                            rs.getString("Notes")
+                            rs.getString("Notes"),
+                            rs.getString("FirstName"),
+                            rs.getString("LastName")
                     };
                     appointments.add(transRow);
                 }
@@ -45,6 +51,254 @@ public class DoctorView extends JFrame {
         return appointments;
     }
 
+    public void addNote(String note, int appointmentID){
+        String query = "update appointments " +
+                "set Notes = ? " +
+                "where AppointmentID = ?;";
+        try {
+            // Connect to the database
+            Connection connection = DriverManager.getConnection(url, uid, pw);
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            // Set the values in the query
+            statement.setString(1, note);
+            statement.setString(2, String.valueOf(appointmentID));
+            // Execute the query
+            int rowsInserted = statement.executeUpdate();
+
+            // Check if the insert was successful
+            if (rowsInserted > 0) {
+                System.out.println("Appointment modified succesfully");
+            }
+
+            // Close the connection
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void modifyAppointmentWindow(int userID){
+        Frame frame = new Frame();
+        frame.setTitle("Add note");
+        frame.setSize(500, 300);
+        frame.setLayout(new GridBagLayout());
+        frame.setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+        GridBagConstraints gbc = new GridBagConstraints();
+        frame.add(new Label("Enter the appointment ID and a note for this appointment:"), gbc);
+
+        gbc.gridwidth = 1;
+
+        TextField AppointmentID = new TextField(20);
+        TextField Note = new TextField(20);
+
+
+        TextField[] textFields = {AppointmentID, Note};
+        String[] labels = {"Enter Appointment ID:", "Note:"};
+
+        for (int i = 0; i < textFields.length; i++) {
+            gbc.gridx = 0;
+            gbc.gridy = i + 1;
+            frame.add(new Label(labels[i]), gbc);
+
+            gbc.gridx = 1;
+            frame.add(textFields[i], gbc);
+        }
+
+
+
+        Button submitButton = new Button("Submit");
+        gbc.gridx = 0;
+        gbc.gridy = 11;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        frame.add(submitButton, gbc);
+        gbc.gridwidth = 1;
+        Button closeButton = new Button("Close");
+        gbc.gridx = 0;
+        gbc.gridy = 12;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        closeButton.addActionListener(e -> frame.dispose());
+        frame.add(closeButton, gbc);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String AppointmentIDInput = AppointmentID.getText();
+                String NoteInput = Note.getText();
+                try {
+                    addNote(NoteInput, Integer.parseInt(AppointmentIDInput));
+                }catch (Exception NumberFormatException) {
+                    System.out.println("invalid ID input");
+                }
+                }
+        });
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                frame.dispose(); // Close the admin window
+                setVisible(true); // Show the login window again
+            }
+        });
+
+    }
+
+    public void addAppointmentQuery(int UserID, String PatientID, String Service, String DateTime, String Status, String Notes) {
+        String query = "INSERT INTO appointments (Patient, User, Service, DateTime, Status, Notes) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            // Connect to the database
+            Connection connection = DriverManager.getConnection(url, uid, pw);
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            // Set the values in the query
+            statement.setString(1, PatientID);
+            statement.setString(2, String.valueOf(UserID));
+            statement.setString(3, Service);
+            statement.setString(4, DateTime);
+            statement.setString(5, Status);
+            statement.setString(6, Notes);
+            // Execute the query
+            int rowsInserted = statement.executeUpdate();
+
+            // Check if the insert was successful
+            if (rowsInserted > 0) {
+                System.out.println("Appointment added successfully");
+            }
+
+            // Close the connection
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        }
+
+    public void addAppointmentWindow(int userID){
+        Frame frame = new Frame();
+        frame.setTitle("Add Appointment");
+        frame.setSize(400, 600);
+        frame.setLayout(new GridBagLayout());
+        frame.setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        frame.add(new Label("Input data for a new appointment:"), gbc);
+
+        gbc.gridwidth = 1;
+
+        TextField PatientID = new TextField(20);
+        TextField Service = new TextField(20);
+        TextField DateTime = new TextField(20);
+        TextField Status = new TextField(20);
+        TextField Notes = new TextField(20);
+
+
+
+        TextField[] textFields = {PatientID, Service, DateTime, Status, Notes};
+        String[] labels = {"Enter PatientID:", "Service:", "Date and Time:", "Status:", "Notes:"};
+
+        for (int i = 0; i < textFields.length; i++) {
+            gbc.gridx = 0;
+            gbc.gridy = i + 1;
+            frame.add(new Label(labels[i]), gbc);
+
+            gbc.gridx = 1;
+            frame.add(textFields[i], gbc);
+        }
+
+
+
+        Button submitButton = new Button("Submit");
+        gbc.gridx = 0;
+        gbc.gridy = 11;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        frame.add(submitButton, gbc);
+        gbc.gridwidth = 1;
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String PacientIDInput = PatientID.getText();
+                String ServiceInput = Service.getText();
+                String DateTimeInput = DateTime.getText();
+                String StatusInput = Status.getText();
+                String NotesInput = Notes.getText();
+
+                //InsertPatient(CNPInput, FirstNameInput, LastNameInput, AddressInput, EmailInput, PhoneInput);
+            }
+        });
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void AppointmentsFound(int ID) {
+        List<String[]> appointments = getAppointments(ID);
+        Frame frame = new Frame("Search Results");
+        if (appointments.isEmpty()) {
+            frame.setSize(200, 200);
+            frame.setVisible(true);
+            Label notFoundLabel = new Label("You don't have any appointments!");
+            frame.add(notFoundLabel);
+            Button ExitButton = new Button("Exit");
+            ExitButton.addActionListener(e -> frame.dispose());
+            frame.add(ExitButton);
+        }
+        else {
+            TextArea textArea = new TextArea();
+            textArea.setBounds(30, 40, 800, 300);
+            textArea.setEditable(false);
+
+            // Add column names as the header
+            textArea.append(String.format("%-10s %-15s %-15s %-15s %-15s %-15s %-25s %-25s %-25s\n",
+                    "AppointmentID", "Patient", "User", "Service", "Date Time", "Status", "Notes", "First Name", "Last Name"));
+
+            // Add data rows to the TextArea
+            for (String[] row : appointments) {
+                textArea.append(String.format("%-10s %-15s %-15s %-15s %-15s %-15s %-25s %-25s %-25s\n",
+                        row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]));
+            }
+
+            // Add the TextArea to the frame
+            frame.add(textArea);
+            Button logoutButton = new Button("Close");
+            logoutButton.setBounds(350, 350, 100, 30); // Set position and size
+            logoutButton.addActionListener(e -> {
+                frame.dispose(); // Close the admin window
+                setVisible(true); // Show the login window again
+            });
+            frame.add(logoutButton);
+            // Set the layout and size of the frame
+            frame.setLayout(null);
+            frame.setSize(900, 400);
+
+            frame.setVisible(true);
+            addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent we) {
+                    System.exit(0);
+                }
+            });
+        }
+    }
 
 
     public DoctorView(int userID, String password) {
@@ -103,6 +357,13 @@ public class DoctorView extends JFrame {
 
         doctorFrame.add(showDataButton);
 
+        Button showAppointmentsButton = new Button("Show Appointments");
+        showAppointmentsButton.addActionListener(e -> AppointmentsFound(userID));
+        doctorFrame.add(showAppointmentsButton);
+
+        Button AddNote = new Button("Add Note to an appointment");
+        AddNote.addActionListener(e -> modifyAppointmentWindow(userID));
+        doctorFrame.add(AddNote);
 
         // Add a logout button
         Button logoutButton = new Button("Logout");
